e8911516df98250662d0c3fb8dfc19bb2b69a2d6
Martin Lowe <martinlowe673@gmail.com>
2024-09-03 07:46:15 +0100

    providers

---
+++ components/providers.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import {ThemeProvider, useTheme} from "next-themes"
+import { ReactNode } from "react"
+
+const providers = ({children}:{children:ReactNode}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default providers
